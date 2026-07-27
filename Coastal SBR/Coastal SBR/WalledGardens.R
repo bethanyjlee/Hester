5695f14a208e77149278db512cced3fb11c31dbf
bethanyjlee <bethanylee@ufl.edu>
2026-07-27 15:09:33 -0400

    walled gardens experiment added

---
+++ Coastal SBR/Coastal SBR/WalledGardens.R
@@ -0,0 +1,247 @@
+rm(list=ls())
+
+
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+
+dat<-read.csv("WalledGardens.csv")
+
+long_dat <- dat %>%
+  pivot_longer(
+    cols = c(SeedlingsQ1, AdultsQ1,
+             SeedlingsQ2.5, AdultsQ2.5,
+             SeedlingsQ4, AdultsQ4),
+    names_to = c("LifeStage", "Quadrat"),
+    names_pattern = "(Seedlings|Adults)Q(.+)",
+    values_to = "Count"
+  ) %>%
+  mutate(
+    Quadrat = factor(Quadrat,
+                     levels = c("1", "2.5", "4")),
+    Walled = factor(Walled,
+                    levels = c("No", "Yes"))
+  )
+
+head(long_dat)
+
+glm(Count ~ Walled, family = poisson(), 
+    data = filter(long_dat, LifeStage == "Seedlings"))
+
+library(glmmTMB)
+
+mod<-glmmTMB(
+  Count ~ Walled + (1 | `Number..E.to.W.`),
+  family = poisson(),
+  data = filter(long_dat, LifeStage == "Seedlings")
+)
+
+car::Anova(mod)
+
+library(ggplot2)
+
+seedlings <- long_dat %>%
+  filter(LifeStage == "Seedlings")
+
+ggplot(seedlings,
+       aes(x = Walled, y = Count, fill = Walled)) +
+  geom_boxplot(width = 0.55,
+               alpha = 0.6,
+               outlier.shape = NA) +
+  geom_jitter(width = 0.08,
+              size = 2.5,
+              alpha = 0.8) +
+  labs(
+    x = NULL,
+    y = "Seedling abundance"
+  ) +
+  theme_classic() +
+  theme(
+    legend.position = "none",
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 13)
+  )
+
+ggplot(seedlings,
+       aes(x = Walled, y = Count, fill = Walled)) +
+  stat_summary(fun = mean,
+               geom = "bar",
+               width = 0.6,
+               color = "black") +
+  stat_summary(fun.data = mean_se,
+               geom = "errorbar",
+               width = 0.15,
+               linewidth = 0.7) +
+  geom_jitter(width = 0.08,
+              size = 2,
+              alpha = 0.7) +
+  labs(
+    x = "Walled or no?",
+    y = "Mean seedling abundance"
+  ) +
+  theme_classic() +
+  theme(
+    legend.position = "none",
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 13)
+  )
+
+ggplot(seedlings,
+       aes(Walled, Count, color = Walled)) +
+  geom_jitter(width = 0.12,
+              size = 2.5,
+              alpha = 0.8) +
+  stat_summary(fun = mean,
+               geom = "point",
+               size = 5,
+               color = "black") +
+  stat_summary(fun.data = mean_se,
+               geom = "errorbar",
+               width = 0.12,
+               color = "black") +
+  labs(
+    x = NULL,
+    y = "Seedling abundance"
+  ) +
+  theme_classic() +
+  theme(
+    legend.position = "none"
+  )
+
+library(dplyr)
+
+seedlings <- long_dat %>%
+  filter(LifeStage == "Seedlings")
+
+seedlings %>%
+  group_by(Walled) %>%
+  summarise(
+    mean = mean(Count),
+    sd = sd(Count),
+    se = sd(Count)/sqrt(n()),
+    median = median(Count),
+    min = min(Count),
+    max = max(Count),
+    n = n()
+  )
+
+
+mod.nb <- glmmTMB(
+  Count ~ Walled + (1 | Number..E.to.W.),
+  family = nbinom2(),
+  data = filter(long_dat, LifeStage == "Seedlings")
+)
+
+summary(mod.nb)
+AIC(mod, mod.nb)
+
+
+
+
+
+
+##### Survival of species #####
+library(tidyverse)
+
+surv <- dat %>%
+  select(
+    Garden = `Number..E.to.W.`,
+    Walled,
+    Distichlis,
+    Frankenia,
+    Jaumea
+  ) %>%
+  pivot_longer(
+    cols = c(Distichlis, Frankenia, Jaumea),
+    names_to = "Species",
+    values_to = "Alive"
+  ) %>%
+  mutate(
+    Dead = 5 - Alive,
+    Survival = Alive / 5,
+    Garden = factor(Garden),
+    Walled = factor(Walled, levels = c("No", "Yes")),
+    Species = factor(Species)
+  )
+
+head(surv)
+
+library(glmmTMB)
+
+mod_surv <- glmmTMB(
+  cbind(Alive, Dead) ~ Walled * Species +
+    (1 | Garden),
+  family = binomial,
+  data = surv
+)
+
+summary(mod_surv)
+car::Anova(mod_surv)
+
+library(ggplot2)
+
+ggplot(surv,
+       aes(x = Walled, y = Survival, fill = Walled)) +
+  stat_summary(fun = mean,
+               geom = "bar",
+               width = 0.7,
+               color = "black") +
+  stat_summary(fun.data = mean_se,
+               geom = "errorbar",
+               width = 0.2) +
+  geom_jitter(width = 0.08,
+              size = 2,
+              alpha = 0.7) +
+  facet_wrap(~Species) +
+  scale_y_continuous(limits = c(0, 1)) +
+  labs(
+    x = "Walled or No?",
+    y = "Survival proportion"
+  ) +
+  theme_classic() +
+  theme(
+    legend.position = "none",
+    strip.background = element_blank(),
+    strip.text = element_text(face = "bold")
+  )
+
+mod_surv2 <- glmmTMB(
+  cbind(Alive, Dead) ~ Walled + Species +
+    (1 | Garden),
+  family = binomial,
+  data = surv
+)
+
+summary(mod_surv2)
+car::Anova(mod_surv2)
+
+library(emmeans)
+
+emmeans(mod_surv2, pairwise ~ Species, type = "response")
+
+emmeans(mod_surv2, pairwise ~ Walled, type = "response")
+
+## analyze each species seprately
+mod.frank <- glmmTMB(
+  cbind(Alive, Dead) ~ Walled + (1|Garden),
+  family = binomial,
+  data = filter(surv, Species == "Frankenia")
+)
+
+car::Anova(mod.frank)
+
+mod.dist <- glmmTMB(
+  cbind(Alive, Dead) ~ Walled + (1|Garden),
+  family = binomial,
+  data = filter(surv, Species == "Distichlis")
+)
+
+car::Anova(mod.dist)
+
+mod.jaum <- glmmTMB(
+  cbind(Alive, Dead) ~ Walled + (1|Garden),
+  family = binomial,
+  data = filter(surv, Species == "Jaumea")
+)
+
+car::Anova(mod.jaum)
